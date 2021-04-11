a219f907b21dcee61c730a78b8fb6790ace9a800
AquisTech <anand.bait@gmail.com>
2021-04-10 21:20:08 -0400

    Bugfix#3 Fix ISD codes are displayed multiple times

--- lib/has_contact_number/helper_methods.rb
+++ lib/has_contact_number/helper_methods.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 module ActionView::Helpers::FormOptionsHelper
 
+  OPTIONS_FOR_ISD_CODES_SELECT = IsoCountryCodes.all.map{ |icc|
+    {
+      code: icc.calling_code,
+      code_to_db: icc.calling_code.delete('+').rjust(4, '0')
+    }
+  }
+  OPTIONS_FOR_ISD_CODES_SELECT_BY_CODE = OPTIONS_FOR_ISD_CODES_SELECT.uniq.map do |record|
+    [record[:code], record[:code_to_db]]
+  end
+
   def options_for_isd_code_select
-    @isd_code_options ||= IsoCountryCodes.all.map do |record|
-      [record.calling_code, record.calling_code.delete('+').rjust(4, '0')]
-    end
+    OPTIONS_FOR_ISD_CODES_SELECT_BY_CODE
   end
 
 end
